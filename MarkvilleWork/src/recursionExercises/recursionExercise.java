0e7777ace9dfe80fc9cbe8ff7d6b3a7c0c1fa12b
Estarguan <estarguan1111@gmail.com>
2024-09-23 14:09:24 -0400

    i dont fucking know what changes its making

--- MarkvilleWork/src/recursionExercises/recursionExercise.java
+++ MarkvilleWork/src/recursionExercises/recursionExercise.java
@@ -2,4 +2,25 @@ package recursionExercises;
 
 public class recursionExercise {
 
+	public static int question1(int customer) {
+		if (customer == 0) {
+			return 0;
+			
+		}
+		if(customer == 1) {
+			return 1;
+		}
+		if (customer % 10 == 0) {
+		
+			return 3+question1(customer-1);
+		}if (customer % 2 == 0) {
+			return 1 + question1(customer-1);
+		}if (customer%2 == 1) {
+			return 2 + question1(customer-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
