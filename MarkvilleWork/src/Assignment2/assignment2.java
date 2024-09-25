35f11195a0292c75bf3b12ac1a0f1ec05bf20d9f
Estarguan <estarguan1111@gmail.com>
2024-09-25 18:01:33 -0400

    adding assignment 2

---
+++ MarkvilleWork/src/Assignment2/assignment2.java
@@ -0,0 +1,65 @@
+package Assignment2;
+import java.util.*;
+import java.io.*;
+
+public class assignment2 {
+
+	public static int dfs(char[][] grid, int minSteps, int steps, int r, int c, int[][] dir,int row, int col,boolean[][] visited) {
+		if (grid[r][c] == 'S') {
+			minSteps = Math.min(steps,minSteps);
+			return minSteps;
+		}
+		for (int i = 0; i < 4; i++) {
+			int changeRow = dir[i][0];
+			int changeCol = dir[i][1];
+			int tempR = r+changeRow, tempC = c+changeCol;
+			if (tempR < 0 || changeCol < 0 || tempR >=row || tempC >= col) {
+				continue;
+			}if (visited[tempR][tempC]) {
+				continue;
+			}if (grid[tempR][tempC] == 'X') {
+				continue;
+			}if (grid[tempR][tempC] == '-' || grid[tempR][tempC] == 'S') {
+				visited[tempR][tempC] = true;
+				minSteps =  dfs(grid,minSteps,steps+1,tempR,tempC,dir,row,col,visited);
+				visited[tempR][tempC] = false;
+			}
+		}
+		return minSteps;
+	}
+	public static void main(String[] args)throws IOException {
+		//To Do: Add the number of grids first inputline
+		int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int testCases = Integer.parseInt(in.readLine());
+		for (int z = 0; z < testCases; z++) {
+			int row = Integer.parseInt(in.readLine());
+			int col = Integer.parseInt(in.readLine());
+			char grid[][] = new char[row][col];
+			boolean visited[][] = new boolean[row][col];
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
+					visited[i][j] = false;
+				}
+			}
+			visited[0][0] = true;
+			for (int i = 0 ; i < row; i++) {
+				String line = in.readLine();
+				for (int j = 0; j < col; j++) {
+					grid[i][j] = line.charAt(j);
+				}
+			}
+			int steps = dfs(grid,Integer.MAX_VALUE,0,0,0,dir,row,col,visited);
+			if (steps == Integer.MAX_VALUE) {
+				System.out.println("No path");
+			}else {
+				System.out.println(steps);
+			}
+			
+			
+		}
+		
+
+	}
+
+}
--- MarkvilleWork/src/recursionExercises/recursionExercise.java
+++ MarkvilleWork/src/recursionExercises/recursionExercise.java
@@ -6,29 +6,6 @@ import java.io.InputStreamReader;
 
 public class recursionExercise {
 
-<<<<<<< HEAD
-	public static int question1(int customer) {
-		if (customer == 0) {
-			return 0;
-			
-		}
-		if(customer == 1) {
-			return 1;
-		}
-		if (customer % 10 == 0) {
-		
-			return 3+question1(customer-1);
-		}if (customer % 2 == 0) {
-			return 1 + question1(customer-1);
-		}if (customer%2 == 1) {
-			return 2 + question1(customer-1);
-		}
-	}
-	
-	public static void main(String[] args) {
-		
-	}
-=======
     public static int bubbles(int customers){
         if (customers <= 0)return 0;
         if (customers == 1) return 2;
@@ -155,5 +132,4 @@ public class recursionExercise {
         }
 
     }
->>>>>>> refs/remotes/origin/master
 }
