d61c99114b7cb79c5d01858597f5d7686747568c
Estar Guan <estarguan1111@gmail.com>
2024-09-25 18:19:57 -0400

    Adding the string path component to assignment 2

--- MarkvilleWork/src/Assignment2/assignment2.java
+++ MarkvilleWork/src/Assignment2/assignment2.java
@@ -3,9 +3,12 @@ import java.util.*;
 import java.io.*;
 
 public class assignment2 {
-
-	public static int dfs(char[][] grid, int minSteps, int steps, int r, int c, int[][] dir,int row, int col,boolean[][] visited) {
+	public static String minPath = "";
+	public static int dfs(char[][] grid, int minSteps, int steps, int r, int c, int[][] dir,int row, int col,boolean[][] visited, String curPath) {
 		if (grid[r][c] == 'S') {
+			if (steps < minSteps){
+				minPath = curPath;
+			}
 			minSteps = Math.min(steps,minSteps);
 			return minSteps;
 		}
@@ -20,18 +23,25 @@ public class assignment2 {
 			}if (grid[tempR][tempC] == 'X') {
 				continue;
 			}if (grid[tempR][tempC] == '-' || grid[tempR][tempC] == 'S') {
+				if (i == 0)curPath+="S ";
+				if (i==1)curPath+="N ";
+				if (i==2)curPath+="E ";
+				if (i==3)curPath+="W ";
 				visited[tempR][tempC] = true;
-				minSteps =  dfs(grid,minSteps,steps+1,tempR,tempC,dir,row,col,visited);
+				minSteps =  dfs(grid,minSteps,steps+1,tempR,tempC,dir,row,col,visited,curPath);
 				visited[tempR][tempC] = false;
+				curPath = curPath.substring(0,curPath.length()-2);
 			}
 		}
 		return minSteps;
 	}
 	public static void main(String[] args)throws IOException {
-		//To Do: Add the number of grids first inputline
+		//To Do: Save the shortest path in string form
 		int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int testCases = Integer.parseInt(in.readLine());
+
+
 		for (int z = 0; z < testCases; z++) {
 			int row = Integer.parseInt(in.readLine());
 			int col = Integer.parseInt(in.readLine());
@@ -49,11 +59,12 @@ public class assignment2 {
 					grid[i][j] = line.charAt(j);
 				}
 			}
-			int steps = dfs(grid,Integer.MAX_VALUE,0,0,0,dir,row,col,visited);
+			int steps = dfs(grid,Integer.MAX_VALUE,0,0,0,dir,row,col,visited, "");
 			if (steps == Integer.MAX_VALUE) {
 				System.out.println("No path");
 			}else {
 				System.out.println(steps);
+				System.out.println(minPath);
 			}
 			
 			
